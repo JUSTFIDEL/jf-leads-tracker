8d41b9b40bb1e7e996ace3a93019f5440aa29446
JUSTFIDEL <fidenki1429@gmail.com>
2023-02-25 16:34:24 +0100

    Logo Update

--- public/index.js
+++ public/index.js
@@ -2,17 +2,25 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-let leads = ["wwwwww", "asdfghjk"]
+let leads = []
 
 inputBtn.addEventListener("click", () => {
 	leads.push(inputEl.value)
-
-	// inputEl.value = ''
+	inputEl.value = ""
+	renderLeads()
 })
 
 let listItems = ""
-for (let i = 0; i < leads.length; i++) {
-	listItems += "<li>" + leads[i] + "</li>"
-}
 
-ulEl.innerHTML = listItems
+function renderLeads() {
+	for (let i = 0; i < leads.length; i++) {
+		listItems += `
+            <li>
+                  <a target='_blank' href='${leads[i]}'> 
+                        ${leads[i]}
+                  </a>
+            </li>`
+	}
+
+	ulEl.innerHTML = listItems
+}
